764406674e55cee59535c65e8186611bcff40353
Salman <salmanmitha@gmail.com>
2020-11-18 17:39:58 +0530

    feat: make merge_method configurable

---
+++ src/util.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core')
+
+const { logWarning } = require('./log')
+
+const mergeMethods = {
+  merge: 'merge',
+  squash: 'squash',
+  rebase: 'rebase'
+}
+
+const getMergeMethod = () => {
+  const input = core.getInput('merge-method')
+
+  if (!input || !mergeMethods[input]) {
+    logWarning('merge-method input is ignored because it is malformed, defaulting to `squash`.')
+    return mergeMethods.squash
+  }
+
+  return mergeMethods[input]
+}
+
+exports.getInputs = () => ({
+  GITHUB_TOKEN: core.getInput('github-token', { required: true }),
+  MERGE_METHOD: getMergeMethod(),
+  EXCLUDE_PKGS: core.getInput('exclude') || []
+})
