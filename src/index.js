717210c45b721b6289d5f4c269bd303f695d3269
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-11-25 12:18:04 +0100

    clean unused code

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@
 
 const core = require('@actions/core')
 const github = require('@actions/github')
-const fetch = require('node-fetch')
 
 const githubActionClient = require('./github-action-client')
-
 const checkTargetMatchToPR = require('./checkTargetMatchToPR')
 const { logInfo, logWarning, logError } = require('./log')
 const { getInputs } = require('./util')
@@ -17,12 +15,10 @@ const {
   EXCLUDE_PKGS,
   MERGE_COMMENT,
   APPROVE_ONLY,
-  API_URL,
   TARGET,
   PR_NUMBER,
 } = getInputs()
 
-const GITHUB_APP_URL = 'https://github.com/apps/dependabot-merge-action'
 
 async function run() {
   try {
@@ -36,13 +32,11 @@ async function run() {
 
     const client = githubActionClient(GITHUB_TOKEN)
 
-
-    // TODO do i have the right permissions?
+    // TODO do i have the right GH permissions?
 
     const pr = pull_request || (await client.getPullRequest(PR_NUMBER))
 
     const isDependabotPR = pr.user.login === 'dependabot[bot]'
-
     if (!isDependabotPR) {
       return logWarning('Not a dependabot PR, skipping.')
     }
@@ -57,25 +51,16 @@ async function run() {
 
     // dependabot branch names are in format "dependabot/npm_and_yarn/pkg-0.0.1"
     const pkgName = pr.head.ref.split('/').pop().split('-').shift()
-
     if (EXCLUDE_PKGS.includes(pkgName)) {
       return logInfo(`${pkgName} is excluded, skipping.`)
     }
 
     await client.approvePullRequest(pr.number, MERGE_COMMENT)
     if (APPROVE_ONLY) {
-      return 'Approving only'
+      return logInfo('Approving only')
     }
 
     await client.mergePullRequest(pr.number, MERGE_METHOD)
-
-
-    // if (!response.ok) {
-    //   throw new Error(
-    //     `Request failed with status code ${response.status}: ${responseText}`
-    //   )
-    // }
-
     logInfo('Dependabot merge completed')
   } catch (error) {
     core.setFailed(error.message)
