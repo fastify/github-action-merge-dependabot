f6945d54958f4da36502794dd1b9591ac2184ebf
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-12-13 18:01:31 +0100

    hotfix: migration guide link (#131)
    * hotfix: release link into upgrade message
    
    * add build
    
    * link to the readme

--- src/action.js
+++ src/action.js
@@ -53,7 +53,7 @@ module.exports = async function run() {
     const { name: pkgName, version } = getPackageDetails(pr)
     const upgradeMessage = `Cannot automerge github-action-merge-dependabot ${version} major release.
   Read how to upgrade it manually:
-  https://github.com/fastify/github-action-merge-dependabot/releases/tag/v${version}`
+  https://github.com/fastify/github-action-merge-dependabot#how-to-upgrade-from-2x-to-new-3x`
 
     if (EXCLUDE_PKGS.includes(pkgName)) {
       return logInfo(`${pkgName} is excluded, skipping.`)
