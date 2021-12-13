f682aefa3c665bbb2c2256cfa5097337df7660f2
Manuel Spigolon <manuel.spigolon@nearform.com>
2021-12-13 17:54:39 +0100

    link to the readme

--- dist/index.js
+++ dist/index.js
@@ -9076,7 +9076,7 @@ module.exports = async function run() {
     const { name: pkgName, version } = getPackageDetails(pr)
     const upgradeMessage = `Cannot automerge github-action-merge-dependabot ${version} major release.
   Read how to upgrade it manually:
-  https://github.com/fastify/github-action-merge-dependabot/releases/tag/v3.0.0`
+  https://github.com/fastify/github-action-merge-dependabot#how-to-upgrade-from-2x-to-new-3x`
 
     if (EXCLUDE_PKGS.includes(pkgName)) {
       return logInfo(`${pkgName} is excluded, skipping.`)
