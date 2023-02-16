b6fe1e91648c4e74398e9001b2712cf55a458336
Samppa Valkama <samppa.valkama@qvik.fi>
2023-02-16 09:00:46 +0200

    fixup! Fix GHES support

--- dist/index.js
+++ dist/index.js
@@ -3111,7 +3111,7 @@ exports.getInputs = inputs => {
     USE_GITHUB_AUTO_MERGE: /true/i.test(inputs['use-github-auto-merge']),
     TARGET: mapUpdateType(inputs['target']),
     PR_NUMBER: inputs['pr-number'],
-    SKIP_COMMIT_VERIFICATION: inputs['skip-commit-verification'],
+    SKIP_COMMIT_VERIFICATION: /true/i.test(inputs['skip-commit-verification']),
   }
 }
 
