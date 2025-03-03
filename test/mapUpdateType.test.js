1d240bcdff551b030b7686324e870117660dff5d
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2025-03-03 20:45:44 +0100

    feat: mapUpdateType

--- test/mapUpdateType.test.js
+++ test/mapUpdateType.test.js
@@ -1,18 +1,18 @@
 'use strict'
-const tap = require('tap')
+const { test } = require('node:test')
 
 const { updateTypes, mapUpdateType } = require('../src/mapUpdateType')
 
-tap.test('mapUpdateType', async t => {
+test('mapUpdateType', async t => {
   t.test(
     'should return the updateType or any if invalid or missing',
     async t => {
-      t.equal(mapUpdateType('major'), updateTypes.major)
-      t.equal(mapUpdateType('minor'), updateTypes.minor)
-      t.equal(mapUpdateType('patch'), updateTypes.patch)
-      t.equal(mapUpdateType('bad_input'), updateTypes.any)
-      t.equal(mapUpdateType(), updateTypes.any)
-      t.equal(mapUpdateType('any'), updateTypes.any)
+      t.assert.deepStrictEqual(mapUpdateType('major'), updateTypes.major)
+      t.assert.deepStrictEqual(mapUpdateType('minor'), updateTypes.minor)
+      t.assert.deepStrictEqual(mapUpdateType('patch'), updateTypes.patch)
+      t.assert.deepStrictEqual(mapUpdateType('bad_input'), updateTypes.any)
+      t.assert.deepStrictEqual(mapUpdateType(), updateTypes.any)
+      t.assert.deepStrictEqual(mapUpdateType('any'), updateTypes.any)
     }
   )
 })
